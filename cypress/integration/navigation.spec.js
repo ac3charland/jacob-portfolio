f4b59f9052b7fce88afce167ffb38754085dabf0
Alex Charland <ac3charland@gmail.com>
2020-08-16 06:49:31 -0500

    Add unit testing for string validation. Add Cypress coverage for new nav bar buttons.

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,24 +1,73 @@
 import NavBar from '../page/nav-bar'
 import HomePage from '../page/home-page'
 import LessonPage from '../page/lesson-page'
+import ContactPage from '../page/contact-page'
 
 context('Page Navigation', () => {
     beforeEach(() => {
         cy.server()
     })
-
-    it('navigates to proper pages with navbar links', () => {
+    
+    it('navigates from home page to proper pages with navbar links', () => {
         cy.visit('/')
         cy.get(HomePage.wrapper)
 
         cy.get(NavBar.link).eq(0).click()
-
         cy.url().should('contain', '/lessons')
         cy.get(LessonPage.wrapper)
 
         cy.get(NavBar.homeLink).click()
-
         cy.url().should('contain', '/')
         cy.get(HomePage.wrapper)
+
+        cy.get(NavBar.link).eq(1).click()
+        cy.url().should('contain', '/contact')
+        cy.get(ContactPage.wrapper)
+
+        cy.go('back')
+        cy.url().should('contain', '/')
+        cy.get(HomePage.wrapper)
+    })
+
+    it('navigates from lesson page to proper pages with navbar links', () => {
+        cy.visit('/lessons')
+        cy.get(LessonPage.wrapper)
+
+        cy.get(NavBar.link).eq(1).click()
+        cy.url().should('contain', '/contact')
+        cy.get(ContactPage.wrapper)
+
+        cy.get(NavBar.link).eq(0).click()
+        cy.url().should('contain', '/lessons')
+        cy.get(LessonPage.wrapper)
+
+        cy.get(NavBar.homeLink).click()
+        cy.url().should('contain', '/')
+        cy.get(HomePage.wrapper)
+
+        cy.go('back')
+        cy.url().should('contain', '/lessons')
+        cy.get(LessonPage.wrapper)
+    })
+
+    it('navigates from contact page to proper pages with navbar links', () => {
+        cy.visit('/contact')
+        cy.get(ContactPage.wrapper)
+
+        cy.get(NavBar.link).eq(0).click()
+        cy.url().should('contain', '/lessons')
+        cy.get(LessonPage.wrapper)
+
+        cy.get(NavBar.link).eq(1).click()
+        cy.url().should('contain', '/contact')
+        cy.get(ContactPage.wrapper)
+
+        cy.get(NavBar.homeLink).click()
+        cy.url().should('contain', '/')
+        cy.get(HomePage.wrapper)
+
+        cy.go('back')
+        cy.url().should('contain', '/contact')
+        cy.get(ContactPage.wrapper)
     })
 })
