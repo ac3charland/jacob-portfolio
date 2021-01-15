38665fc1293762d9846b5dd7519430a5eb2d07c6
Alex Charland <ac3charland@gmail.com>
2021-01-15 08:01:38 -0600

    Add item to hero text and reposition

--- src/components/parallax-components/parallax-hero.jsx
+++ src/components/parallax-components/parallax-hero.jsx
@@ -8,6 +8,7 @@ const ParallaxHero = () => (
         <div className={`${cb}__background-layer`} />
         <div className={`${cb}__hero-title-wrapper`}>
             <div className={`${cb}__hero-title`}>PERCUSSIONIST</div>
+            <div className={`${cb}__hero-title`}>RECORDING ARTIST</div>
             <div className={`${cb}__hero-title`}>EDUCATOR</div>
         </div>
     </div>
