33126af6d583d8a56bbe9cf48bfbc98ae5e5c26b
Alex Charland <ac3charland@gmail.com>
2021-01-22 07:14:40 -0600

    Capture calendar backstop images

--- backstop/scenarios.js
+++ backstop/scenarios.js
@@ -22,7 +22,7 @@ module.exports = backstop.config({
         cs({label: 'home-page'}),
         cs({label: 'home-page__bio', onReadyScript: 'scroll-on-homepage.js', args: {selector: '#backstop-bio'}}),
         // TODO - Removing until we can mock calendar response in Backstop
-        // cs({label: 'home-page__calendar', onReadyScript: 'scroll-on-homepage.js', args: {selector: '#backstop-calendar'}}),
+        cs({label: 'home-page__calendar', onReadyScript: 'scroll-on-homepage.js', args: {selector: '#backstop-calendar'}}),
         cs({label: 'home-page__media', onReadyScript: 'scroll-on-homepage.js', args: {selector: '#backstop-media'}}),
         cs({label: 'lessons', onReadyScript: 'navigate-to-page.js', args: {id: '#lessons-link'}}),
         cs({label: 'lessons-form-error', onReadyScript: 'trigger-form-error.js', args: {id: '#lessons-link'}}),
