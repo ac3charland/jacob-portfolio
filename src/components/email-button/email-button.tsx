667a884c3aa1082edb87f942d1b67c41715bb6c1
Alex Charland <ac3charland@gmail.com>
2023-08-14 09:55:53 -0500

    Obscure email address

--- src/components/email-button/email-button.tsx
+++ src/components/email-button/email-button.tsx
@@ -4,9 +4,9 @@ import './email-button.scss'
 const cb = 'email-button'
 
 const EmailButton = (): JSX.Element => (
-  <a href='mailto:jabicknase@gmail.com' className={cb}>
+  <a href='https://tinyurl.com/2386kw39' className={cb}>
     Email Me To Get Started
   </a>
-)
+);
 
 export default EmailButton
